97a57661532ebb54d1a2b822816f65dd1f115c15
hufeng <hufeng@qianm.com>
2015-08-22 14:05:12 +0800

    update

--- apps/home/index.js
+++ apps/home/index.js
@@ -61,6 +61,7 @@ var Home = React.createClass({
         {/*订单*/}
         <TabBarIOS.Item
           title='订单'
+          badge={9}
           icon={require('image!cart-grey')}
           selectedIcon={require('image!cart-blue')}
           selected={this.state.selectedTab === 'order'}
